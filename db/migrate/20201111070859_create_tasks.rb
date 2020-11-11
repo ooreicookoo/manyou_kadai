4594ff91dab7b3d91a289e04f999f32522debaa5
reiko enomoto <ooreicookoo@gmail.com>
2020-11-11 16:25:13 +0900

    それぞれのモデルをmigratre

--- db/migrate/20201111070859_create_tasks.rb
+++ db/migrate/20201111070859_create_tasks.rb
@@ -1,7 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-
+      t.string :title
+      t.text :content
+      t.integer :priority
+      t.integer :status
+      t.date :limit
+      
       t.timestamps
     end
   end
--- db/migrate/20201111070908_create_labels.rb
+++ db/migrate/20201111070908_create_labels.rb
@@ -1,6 +1,8 @@
 class CreateLabels < ActiveRecord::Migration[5.2]
   def change
     create_table :labels do |t|
+      t.integer :user_id
+      t.integer :task_id
 
       t.timestamps
     end
