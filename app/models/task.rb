aee101315f5d479a984a9feae658d8663e52a688
reiko enomoto <ooreicookoo@gmail.com>
2020-11-11 18:33:01 +0900

    taskモデルのバリテーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+  belogs_to :user
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :limit, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
+  belogs_to :user
 end
