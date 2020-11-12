08f92ad4293a809fe6c8e9490afaa96adfb6a1d4
reiko enomoto <ooreicookoo@gmail.com>
2020-11-12 15:27:28 +0900

    taskコントローラ create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,11 @@ class TasksController < ApplicationController
   def index
   end
   def new
+    @task = Task.new
   end
   def create
+    Task.create(params.require(:task).permit(:title, :content, :priority, :status, :limit))
+    redirect_to new_task_path
   end
   def edit
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,8 @@
 class Task < ApplicationRecord
-  belogs_to :user
   validates :title, presence: true
   validates :content, presence: true
   validates :limit, presence: true
   validates :status, presence: true
   validates :priority, presence: true
-  belogs_to :user
+  belongs_to :user
 end
