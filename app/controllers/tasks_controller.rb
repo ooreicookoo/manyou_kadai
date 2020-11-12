1576670fd81a6b3338a391ec34c3c77ed2ae91f8
reiko enomoto <ooreicookoo@gmail.com>
2020-11-12 17:25:45 +0900

    タスク一覧表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,28 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
   def new
     @task = Task.new
   end
   def create
-    Task.create(params.require(:task).permit(:title, :content, :priority, :status, :limit))
-    redirect_to new_task_path
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice:"task追加"
+      else
+        render :new
+      end
+    end
   end
   def edit
+  end
+  def show
+  end
+  private
+  def task_params
+    params.require(:task).permit(:title, :content, :priority, :status, :limit)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,3 @@
 class Task < ApplicationRecord
   validates :title, presence: true
-  validates :content, presence: true
-  validates :limit, presence: true
-  validates :status, presence: true
-  validates :priority, presence: true
-  belongs_to :user
 end
