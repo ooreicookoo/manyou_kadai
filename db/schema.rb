e2bc292cb2e4bbcce8ea09d44b65f1d3378a4d12
reiko enomoto <ooreicookoo@gmail.com>
2020-11-13 17:13:36 +0900

    間違って作ったマイグレファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_070908) do
+ActiveRecord::Schema.define(version: 2020_11_11_070859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "labels", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "task_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
@@ -28,14 +21,6 @@ ActiveRecord::Schema.define(version: 2020_11_11_070908) do
     t.integer "priority", null: false
     t.integer "status", null: false
     t.date "limit", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
