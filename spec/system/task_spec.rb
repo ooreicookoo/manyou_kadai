b27c76808f611286f5455d11cec830be58c6863e
reiko enomoto <ooreicookoo@gmail.com>
2020-11-17 17:30:50 +0900

    view _form エラーメッセージの修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  # let!(:task) { FactoryBot.create(:task, title: 'task') }
   before do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
+    visit tasks_path
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        binding.irb
         fill_in "task_title", with: 'task'
-        fill_in "task_content", with: 'task'
-        click_button "Create Task"
+        fill_in "task_content", with: 'content'
+        click_button "登録"
         expect(page).to have_content 'task'
       end
     end
@@ -25,13 +26,20 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[1]).to have_content 'task2'
+      end
+    end
+  end
   describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
-         task = FactoryBot.create(:task, title: 'task')
-         visit task_path(task.id)
-         expect(page).to have_content 'task'
-       end
-     end
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当のタスクの内容が表示される' do
+        task = FactoryBot.create(:task, title: 'task')
+        visit task_path(task.id)
+        expect(page).to have_content 'task'
+      end
+    end
   end
 end
