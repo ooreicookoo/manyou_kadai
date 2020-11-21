708bbb312476ba60e884314bd36ff56b18a532ad
reiko enomoto <ooreicookoo@gmail.com>
2020-11-21 18:39:02 +0900

    カラムにlimit_date追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_070859) do
+ActiveRecord::Schema.define(version: 2020_11_20_081724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_11_11_070859) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "limit", default: -> { "now()" }, null: false
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
