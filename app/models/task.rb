75afa621ff8026c05590582b0691801327082d17
reiko enomoto <ooreicookoo@gmail.com>
2020-11-25 15:35:16 +0900

    モデル：task.rb　バリテーション追加修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,11 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: {in: 1..100}
+  validates :limit, presence: true
   enum status: { 未着手: 0, 着手: 1, 完了: 2 }
   enum priority: { 高: 0, 中: 1, 低: 2 }
-  scope :title_search, ->(title){ where("title like ?", "%#{title}%") }
+
+  scope :title_search, ->(title){ where("title Like ?", "%#{title}%") }
   scope :status_search, ->(status){ where(status: status) }
   scope :priority_search, ->(priority){ where(priority: priority) }
 
