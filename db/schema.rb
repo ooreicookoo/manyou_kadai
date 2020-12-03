4e6f47538af822ca5d81c92884898a4b0fe012e8
reiko enomoto <ooreicookoo@gmail.com>
2020-12-03 13:25:50 +0900

    Userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_095914) do
+ActiveRecord::Schema.define(version: 2020_12_03_042353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2020_11_21_095914) do
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 0, null: false
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
